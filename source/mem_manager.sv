1ba3e7462c3a57be807b61ebb2ede4b460f332cf
medha <shindem@purdue.edu>
2024-06-18 16:29:36 -0400

    implemented memory manager?

--- source/mem_manager.sv
+++ source/mem_manager.sv
@@ -30,9 +30,27 @@ module mem_manager(
             INIT: if (!rst) next_state = IDLE;  
                   else next_state = INIT; 
             
-            IDLE: if (memRead) next_state = Read_Request; prev_state = Read_Request; data_en = 1;
-                  else if (memWrite) next_state = Write_Request; prev_state = Write_Request; data_en = 1;
+            IDLE: if (memRead) next_state = Read_Request; prev_state = Read_Request;
+                  else if (memWrite) next_state = Write_Request; prev_state = Write_Request;
                   else next_state = IDLE; 
+
+            Read_Request: if (bus_full) next_state = Wait; prev_state = Read_Request;
+                          else next_state = Read;
+            
+            Write_Request: if (bus_full) next_state = Wait; prev_state = Write_Request;
+                          else next_state = Write;
+            
+            Read: address_out = address_in; data_out_BUS = 32'b0; 
+                if (data_en) data_out_CPU = data_in_BUS; data_out_INSTR = 32'b0; // going to MUX
+                if (instr_en) data_out_CPU = 32'b0; data_out_INSTR = data_in_BUS; // going to CU
+                next_state = IDLE;
+            
+            Write: address_out = address_in; data_out_BUS = data_in_CPU; 
+                   data_out_INSTR = 32'b0; data_out_CPU = 32'b0;
+                   next_state = IDLE;
+
+            Default: next_state = IDLE:
+            
         endcase
     end
  endmodule
