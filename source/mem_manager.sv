d307518d226755f4c8de5995624f3bcdb615b7b1
medha <shindem@purdue.edu>
2024-06-18 16:36:21 -0400

    hopefully made mem_manager more readable

--- source/mem_manager.sv
+++ source/mem_manager.sv
@@ -1,56 +1,109 @@
 typedef enum logic [2:0] {
-    INIT = 0; IDLE = 1;
-    Read_Request = 2; Write_Request = 3;
-    Read = 4; Write = 5;
-    Wait = 6;
+    INIT = 3'b000,
+    IDLE = 3'b001,
+    Read_Request = 3'b010,
+    Write_Request = 3'b011,
+    Read = 3'b100,
+    Write = 3'b101,
+    Wait = 3'b110
 } state_t;
 
 module mem_manager(
     // inputs
-    input [31:0] address_in, data_in_CPU, data_in_BUS;
-    input data_en, instr_en, bus_full, memWrite, memRead;
-    input clk, rst;
+    input logic [31:0] address_in, data_in_CPU, data_in_BUS,
+    input logic data_en, instr_en, bus_full, memWrite, memRead,
+    input logic clk, rst,
     // outputs
-    output state_t state;
-    output [31:0] address_out, data_out_CPU, data_out_BUS, data_out_INSTR;
+    output state_t state,
+    output logic [31:0] address_out, data_out_CPU, data_out_BUS, data_out_INSTR
 );
 
     state_t next_state, prev_state;
-    assign address_out = address_in;
-    assign logic bus_empty = (!bus_full);
+    logic bus_empty;
+    assign bus_empty = (!bus_full);
 
-    always_ff @( posedge clk ) begin : startFSM
-        if (rst) begin state <= INIT; end 
-        else begin state <= next_state; end
+    always_ff @(posedge clk || posedge rst) begin : startFSM
+        if (rst) begin
+            state <= INIT;
+        end else begin
+            state <= next_state;
+        end
     end
 
     always_comb begin : changeState
         next_state = state;
-        case(state) 
-            INIT: if (!rst) next_state = IDLE;  
-                  else next_state = INIT; 
+        case(state)
+            INIT: begin 
+                if (!rst) next_state = IDLE;
+                else next_state = INIT;
+            end
             
-            IDLE: if (memRead) next_state = Read_Request; prev_state = Read_Request;
-                  else if (memWrite) next_state = Write_Request; prev_state = Write_Request;
-                  else next_state = IDLE; 
+            IDLE: begin 
+                if (memRead) begin
+                    next_state = Read_Request;
+                    prev_state = Read_Request;
+                end else if (memWrite) begin
+                    next_state = Write_Request;
+                    prev_state = Write_Request;
+                end else begin
+                    next_state = IDLE;
+                end
+            end
 
-            Read_Request: if (bus_full) next_state = Wait; prev_state = Read_Request;
-                          else next_state = Read;
+            Read_Request: begin 
+                if (bus_full) begin
+                    next_state = Wait;
+                end else begin
+                    next_state = Read;
+                end
+            end
             
-            Write_Request: if (bus_full) next_state = Wait; prev_state = Write_Request;
-                          else next_state = Write;
+            Write_Request: begin 
+                if (bus_full) begin
+                    next_state = Wait;
+                end else begin
+                    next_state = Write;
+                end
+            end
             
-            Read: address_out = address_in; data_out_BUS = 32'b0; 
-                if (data_en) data_out_CPU = data_in_BUS; data_out_INSTR = 32'b0; // going to MUX
-                if (instr_en) data_out_CPU = 32'b0; data_out_INSTR = data_in_BUS; // going to CU
-                next_state = IDLE;
+            Read: begin 
+                address_out = address_in;
+                data_out_BUS = 32'b0; 
+                if (data_en) begin
+                    data_out_CPU = data_in_BUS;
+                    data_out_INSTR = 32'b0; // going to MUX
+                end
+                if (instr_en) begin
+                    data_out_CPU = 32'b0;
+                    data_out_INSTR = data_in_BUS; // going to CU
+                end
+                next_state = IDLE; 
+            end
             
-            Write: address_out = address_in; data_out_BUS = data_in_CPU; 
-                   data_out_INSTR = 32'b0; data_out_CPU = 32'b0;
-                   next_state = IDLE;
+            Write: begin 
+                address_out = address_in;
+                data_out_BUS = data_in_CPU;
+                data_out_INSTR = 32'b0;
+                data_out_CPU = 32'b0;
+                next_state = IDLE; 
+            end
 
-            Default: next_state = IDLE:
+            Wait: begin 
+                if (!bus_full) begin
+                    if (prev_state == Read_Request) begin
+                        next_state = Read;
+                    end else if (prev_state == Write_Request) begin
+                        next_state = Write;
+                    end else begin
+                        next_state = IDLE;
+                    end
+                end else begin
+                    next_state = Wait;
+                end
+            end
+
+            default: next_state = IDLE;
             
         endcase
     end
- endmodule
+endmodule
