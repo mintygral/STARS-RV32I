7de8642e8bf91c48a7a1e269e18f1dd9a1a9a65b
Aarynda <77419463+Aarynda@users.noreply.github.com>
2024-06-18 16:11:04 -0400

    adding first test case to tb_control_unit

--- source/control_unit.sv
+++ source/control_unit.sv
@@ -1,13 +1,11 @@
 module control_unit(
-    input logic [31:0] instruction
+    input logic [31:0] instruction,
+    output logic [6:0] opcode, funct7,
+    output logic [2:0] funct3,
+    output logic [4:0] rs1, rs2, rd,
+    output logic [19:0] imm_20,
+    output logic ALU_source //0 means register, 1 means immediate
 );
-    logic [6:0] opcode, funct7;
-    logic [2:0] funct3;
-    logic [4:0] rs1, rs2, rd;
-    logic [11:0] imm_12;
-    logic [19:0] imm_20;
-
-    logic ALU_source; //0 means register, 1 means immediate
 
     always_comb begin
         opcode = instruction[6:0];
@@ -19,7 +17,6 @@ module control_unit(
                     rd = instruction[11:7];
                     rs1 = instruction[19:15];
                     rs2 = instruction[24:20];
-                    imm_12 = 12'b0;
                     imm_20 = 20'b0;
                     ALU_source = 1'b0;
                 end
@@ -30,10 +27,9 @@ module control_unit(
                     funct3 = instruction[14:12];
                     rd = instruction[11:7];
                     rs1 = instruction[19:15];
-                    imm_12 = instruction[31:20];
+                    imm_20 = {8'b0, instruction[31:20]};
                     funct7 = 7'b0;
                     rs2 = 5'b0;
-                    imm_20 = 20'b0;
                     ALU_source = 1'b1;
                 end
             7'b0100011: //s type instructions
@@ -41,10 +37,9 @@ module control_unit(
                     funct3 = instruction[14:12];
                     rs1 = instruction[19:15];
                     rs2 = instruction[24:20];
-                    imm_12 = {instruction[31:25], instruction[11:7]};
+                    imm_20 = {8'b0, instruction[31:25], instruction[11:7]};
                     funct7 = 7'b0;
                     rd = 5'b0;
-                    imm_20 = 20'b0;
                     ALU_source = 1'b1;
                 end
             7'b1100011: //b type instruction
@@ -52,9 +47,9 @@ module control_unit(
                     funct3 = instruction[14:12];
                     rs1 = instruction[19:15];
                     rs2 = instruction[24:20];
-                    imm_12 = {instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
+                    imm_20 = {8'b0, instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
                     funct7 = 7'b0;
-                    imm_20 = 20'b0;
+                    rd = 5'b0;
                     ALU_source = 1'b1;
                 end
             7'b1101111: //j type instruction
@@ -65,7 +60,6 @@ module control_unit(
                     rs2 = 5'b0;
                     funct3 = 3'b0;
                     funct7 = 7'b0;
-                    imm_12 = 12'b0;
                     ALU_source = 1'b1;
                 end
             7'b0110111: //u type instruction
@@ -76,7 +70,6 @@ module control_unit(
                     rs2 = 5'b0;
                     funct3 = 3'b0;
                     funct7 = 7'b0;
-                    imm_12 = 12'b0;
                     ALU_source = 1'b1;
                 end
         endcase
--- source/tb_control_unit.sv
+++ source/tb_control_unit.sv
@@ -1,9 +1,52 @@
 module tb_control_unit;
 
     logic [31:0] instruction;
+    logic [6:0] opcode, funct7;
+    logic [2:0] funct3;
+    logic [4:0] rs1, rs2, rd;
+    logic [19:0] imm_20;
+    logic ALU_source; //0 means register, 1 means immediate
 
-    control_unit cont(.instruction(instruction));
+    control_unit cont(.instruction(instruction), .opcode(opcode), .funct7(funct7), .funct3(funct3), .rs1(rs1), .rs2(rs2), .rd(rd), .imm_20(imm_20), .ALU_source(ALU_source));
 
-    
+    initial begin
+        $dumpfile("sim.vcd");
+        $dumpvars(0, tb_control_unit);
+
+        add_r1_r2_r3;
+
+    end
+
+    task add_r1_r2_r3;
+        instruction = 32'b0000000_00001_00010_000_00011_0110011;
+        #5;
+        check_opcode(7'b0110011);
+        check_funct7(7'b0);
+        check_funct3(3'b0);
+        check_registers(5'b00001, 5'b00010, 5'b00011);
+        check_imm(20'b0);
+    endtask
+
+    task check_opcode(input [6:0] exp_opcode)
+        if(opcode != exp_opcode) $display("Incorrect opcode. Expected opcode: %b, actual opcode: %b", exp_opcode, opcode);
+    endtask
+
+    task check_funct7(input [6:0] exp_funct7)
+        if(funct7 != exp_funct7) $display("Incorrect funct7. Expected funct7: %b, actual funct7: %b", exp_funct7, funct7);
+    endtask
+
+    task check_funct3(input [2:0] exp_funct3)
+        if(funct3 != exp_funct3) $display("Incorrect funct3. Expected funct3: %b, actual funct3: %b", exp_funct3, funct3);
+    endtask
+
+    task check_registers(input [4:0] exp_rs1, exp_rs2, exp_rd)
+        if(rs1 != exp_rs1) $display("Incorrect rs1. Expected rs1: %b, actual rs1: %b", exp_rs1, rs1)
+        if(rs2 != exp_rs2) $display("Incorrect rs2. Expected rs2: %b, actual rs2: %b", exp_rs2, rs2)
+        if(rd != exp_rd) $display("Incorrect rd. Expected rd: %b, actual rd: %b", exp_rd, rd)
+    endtask
+
+    task check_imm(input [20:0] exp_imm)
+        if(imm_20 != exp_imm) $display("Incorrect imm_20. Expected imm_20: %b, actual imm_20: %b", exp_imm, imm_20)
+    endtask
 
 endmodule
