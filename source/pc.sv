a9a09f49fd52ccdde80d41243dda175f22e04f48
gopalak8 <gopalak8@purdue.edu>
2024-06-21 15:38:55 -0400

    Update pc.sv

--- source/pc.sv
+++ source/pc.sv
@@ -1,6 +1,6 @@
 
 module pc(
-    input logic clk, clr, load, inc, ALU_out, Disable,
+    input logic clk, clr, load, inc, Disable, ALU_out,
     input logic [31:0] data, imm_val, 
     output logic [31:0] pc_val 
 );
@@ -8,7 +8,6 @@ module pc(
     logic [31:0] jump_ad;
     logic [31:0] next_pc;
     logic branch_choice;
-    //logic [31:0] next_ad;
 
     // Register 
     always_ff @(posedge clk, negedge clr) begin
@@ -19,41 +18,35 @@ module pc(
 
         else begin
             pc_val <= next_pc;
-            //next_ad <= next_pc;
         end
     end
 
+
    always_comb begin
        next_pc = pc_val;
-       next_line_ad = pc_val + 32'd4;	// Calculate next line address
-       jump_ad = pc_val + imm_val;    // Calculate jump address
-       branch_choice = ALU_out;     // branch choice based on ALU output
+       next_line_ad = pc_val + 32'd4;	// Calculate next line address  
+       jump_ad = next_line_ad + imm_val;    // Calculate jump address (jump and link)
+
 	
         // Mux choice between next line address and jump address
-        if (load) begin
-            if (branch_choice) begin
-		        next_pc = jump_ad;
-	        end
-	
-	        else begin
-		        next_pc = pc_val;
-	        end
-        end
-      
-       else if (inc) begin
-            next_pc= next_line_ad;
-       end
-    //    else begin
-    //         next_pc = data;
-    //    end
-
-       else if (Disable) begin 
+        if (Disable) begin 
 		    next_pc = pc_val; 
 	    end
 
-       
-   end
+        else if (load) begin
+            next_pc = data + next_line_ad;
+        end
+            
+        else if (ALU_out) begin
+		    next_pc = jump_ad ;
+	    end
+	
+        else if (inc) begin
+            next_pc= next_line_ad;
+       end
 
         
+       
+   end       
     
  endmodule
