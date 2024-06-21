b5eb76148d6ae05e93c753ba560b6c4cad36b710
ajstroth <ajstrothkamp@yahoo.com>
2024-06-21 12:52:24 -0700

    Updating register file

--- source/register_file.sv
+++ source/register_file.sv
@@ -2,71 +2,32 @@
 
 module register_file (
     input logic [31:0] reg_write, 
-    input logic [4:0] rd, rs1, rs2, //not sure about literally any bit amounts
+    input logic [4:0] rd, rs1, rs2, 
     input logic clk, rst, write,
-    output reg [31:0] reg1, reg2
+    output logic [31:0] reg1, reg2 //array????
 );
-    reg[31:0] register [31:0]; //correct syntax for array
-    //logic write;
+    reg[31:0][31:0] register; 
+    reg[31:0][31:0] next_register; 
 
-    // always_comb begin
-    //     if (reg_write == 32'b0) begin
-    //         write = 1'b0;
-    //     end else begin
-    //         write = 1'b1;
-    //     end
-    // end
 
-    always @ ( //or should I use initial
-        rs1, 
-        rs2, 
-        register[0], 
-        register[1], 
-        register[2], 
-        register[3],
-        register[4],
-        register[5],
-        register[6], 
-        register[7], 
-        register[8], 
-        register[9],
-        register[10],
-        register[11],
-        register[12],
-        register[13],
-        register[14],
-        register[15],
-        register[16],
-        register[17],
-        register[18],
-        register[19],
-        register[20],
-        register[21],
-        register[22],
-        register[23],
-        register[24],
-        register[25],
-        register[26],
-        register[27],
-        register[28],
-        register[29],
-        register[30],
-        register[31]
-    ) begin
-        reg1 <= register[rs1];
-        reg2 <= register[rs2];
+    //assign register = '{default:'0};
+
+    always_comb begin
+        next_register = register;
+        if (write) begin
+            next_register[rd] = reg_write;
+        end
+        reg1 = register[rs1];
+        reg2 = register[rs2];
     end
 
-    always @ (posedge clk, posedge rst) begin //reset pos or neg or no reset
-        if (rst) begin
-            for (integer i = 0; i <= 31; i++) begin
-                register[i] <= 0;
-            end
+    always_ff @ (posedge clk, negedge rst) begin //reset pos or neg or no reset
+        if (~rst) begin
+            register <= '0;
         end
         else begin
-            if (write) begin
-                register[rd] <= reg_write;
-            end
+            register <= next_register;
+           
         end
     end
 endmodule
--- source/tb_register_file.sv
+++ source/tb_register_file.sv
@@ -11,8 +11,15 @@ module tb_register_file;
     integer tb_test_num;
     string tb_test_name;
 
-    //add outputs!!!
-    register_file regFile(.reg_write(writeData), .clk(clk), .rst(rst), .write(writeEnable), .rd(writeReg), .rs1(readReg1), .rs2(readReg2), .reg1(regOut1), .reg2(regOut2));
+    register_file regFile(.reg_write(writeData), 
+    .clk(clk), 
+    .rst(rst), 
+    .write(writeEnable), 
+    .rd(writeReg), 
+    .rs1(readReg1), 
+    .rs2(readReg2), 
+    .reg1(regOut1), 
+    .reg2(regOut2));
 
     //Toggle clock
     always begin
@@ -24,134 +31,198 @@ module tb_register_file;
 
     initial begin
         
-        $dumpfile("dump.vcd");
+        $dumpfile("sim.vcd");
         $dumpvars(0, tb_register_file);
 
         tb_test_num = -1;
         tb_test_name = "Test Bench Initialization";
-        rst = 1'b0;
+        rst = 1'b1;
         writeEnable = 1'b0;
-        //ADD OTHER VARIABLES
+        writeData = 32'b0;
+        writeReg = 5'b0;
+        readReg1 = 5'b0;
+        readReg2 ='0;
 
         #(0.5);
         
         //////////////////////////
         //Test 0: Power on Reset//
         //////////////////////////
-        $info("Test 0: power on reset");
+        // $info("Test 0: power on reset");
 
         tb_test_num += 1;
         tb_test_name = "Power on reset";
         toggle_rst;
 
         //set inputs to non-reset values
-        writeReg = 5'b0;
-        readReg1 = 5'b1;
-        readReg2 = 5'b1;
-        writeEnable = 1'b0;
+        writeReg = 5'b1;
+        writeData = 32'b1;
+        readReg1 = 5'b0;
+        readReg2 = 5'b0;
+        writeEnable = 1'b1;
         
-        rst = 1'b1;
+        readReg1 = 5'b1;
+        readReg2 = 5'b0;
 
-        #(CLK_PERIOD * 2);
-
-        exp_read1 = 32'b0;
+        exp_read1 = 32'b1;
         exp_read2 = 32'b0;
         check_output(exp_read1, exp_read2);
 
-        rst = 1'b0;
+        writeReg = 5'b00010;
+        writeData = 32'b1;
+        writeEnable = 1'b1;
 
-        //What are the expected outputs when back on
-        exp_read1 = 32'b1; //???
-        exp_read2 = 32'b1; //???
+        readReg2 = 5'b00010;
+
+        exp_read1 = 32'b1;
+        exp_read2 = 32'b1;
         check_output(exp_read1, exp_read2);
 
-        //////////////////////////
-        //Test 1: Test Writing  //
-        //////////////////////////
+        toggle_rst;
+
+        exp_read1 = 32'b0;
+        exp_read2 = 32'b1; //still being written too
+        check_output(exp_read1, exp_read2);
+
+        /////////////////////////////////////////
+        //Test 1: Test Writing to both outputs //
+        /////////////////////////////////////////
         $info("Test 1: Test Writing");
 
         tb_test_num += 1;
         tb_test_name = "Test Writing";
         toggle_rst;
 
+        //set inputs to non-reset values
         writeReg = 5'b1;
+        writeData = 32'b1;
         readReg1 = 5'b0;
         readReg2 = 5'b0;
         writeEnable = 1'b1;
-
-        #(CLK_PERIOD * 2);
-
-        //OUTPUTS????
-        exp_read1 = 5'b0;
-        exp_read2 = 5'b0;
-        check_output(exp_read1, exp_read2);
-
-        /////////////////////////////
-        //Test 2: Nothing going on //
-        /////////////////////////////
-        $info("Test 2: Nothing going on");
-
-        tb_test_num += 1;
-        tb_test_name = "Nothing going on";
-        toggle_rst;
-
-        writeReg = 5'b0;
-        readReg1 = 5'b0;
-        readReg2 = 5'b0;
-        writeEnable = 1'b0;
-
-        #(CLK_PERIOD * 2);
-
         
-        exp_read1 = 5'b0;
-        exp_read2 = 5'b0;
-        check_output(exp_read1, exp_read2);
-
-        //////////////////////////////////////
-        //Test 3: Test Reading/Sending value//
-        //////////////////////////////////////
-        $info("Test 3: Test Reading/Sending value");
-
-        tb_test_num += 1;
-        tb_test_name = "Test Reading/Sending Value";
-        toggle_rst; 
-
-        writeReg = 5'b0;
         readReg1 = 5'b1;
         readReg2 = 5'b1;
-        writeEnable = 1'b0;
 
-        #(CLK_PERIOD * 2);
-
-        //OUYpUYS
-        exp_read1 = 5'b1;
-        exp_read2 = 5'b1;
+        exp_read1 = 32'b1;
+        exp_read2 = 32'b1;
         check_output(exp_read1, exp_read2);
+
+        /////////////////////////////////////////
+        //Test 2: Test Writing to reg1   //
+        /////////////////////////////////////////
+        // $info("Test 2: Test Writing to reg1");
+
+        // tb_test_num += 1;
+        // tb_test_name = "Test Writing one output";
+        // toggle_rst;
+
+        // //set inputs to non-reset values
+        // writeReg = 5'b1;
+        // writeData = 32'b1;
+        // readReg1 = 5'b0;
+        // readReg2 = 5'b0;
+        // writeEnable = 1'b1;
+        
+        // readReg1 = 5'b1;
+        // readReg2 = 5'b0;
+
+        // exp_read1 = 32'b1;
+        // exp_read2 = 32'b0;
+        // check_output(exp_read1, exp_read2);
+
+        /////////////////////////////////////////
+        //Test 3: Test Writing to reg2   //
+        /////////////////////////////////////////
+        // $info("Test 2: Test Writing to reg2");
+
+        // tb_test_num += 1;
+        // tb_test_name = "Test Writing to reg2";
+        // toggle_rst;
+
+        // //set inputs to non-reset values
+        // writeReg = 5'b1;
+        // writeData = 32'b1;
+        // readReg1 = 5'b0;
+        // readReg2 = 5'b0;
+        // writeEnable = 1'b1;
+        
+        // readReg1 = 5'b0;
+        // readReg2 = 5'b1;
+
+        // exp_read1 = 32'b0;
+        // exp_read2 = 32'b1;
+        // check_output(exp_read1, exp_read2);
+
+        /////////////////////////////
+        //Test 4: Nothing going on //
+        /////////////////////////////
+        // $info("Test 2: Nothing going on");
+
+        // tb_test_num += 1;
+        // tb_test_name = "Nothing going on";
+        // toggle_rst;
+
+        // writeReg = 5'b0;
+        // readReg1 = 5'b0;
+        // readReg2 = 5'b0;
+        // writeEnable = 1'b0;
+        // writeData = 32'b0;
+
+        // #(CLK_PERIOD * 1);
+
+        
+        // exp_read1 = 32'b0;
+        // exp_read2 = 32'b0;
+        // check_output(exp_read1, exp_read2);
+
+        //////////////////////////////////////
+        //Test 5: Test Reading/Sending value//
+        //////////////////////////////////////
+        // $info("Test 3: Test Reading/Sending value");
+
+        // tb_test_num += 1;
+        // tb_test_name = "Test Reading/Sending Value";
+        // toggle_rst; 
+
+        // writeReg = 5'b0;
+        // readReg1 = 5'b1;
+        // readReg2 = 5'b1;
+        // writeEnable = 1'b0;
+        // writeData = 32'b0;
+
+        // #(CLK_PERIOD * 1);
+
+        // //OUYpUYS
+        // exp_read1 = 32'b0;
+        // exp_read2 = 32'b0;
+        // check_output(exp_read1, exp_read2);
+
+        $finish;
 
     end
 
 
     task check_output;
-        input logic [4:0] exp_read1, exp_read2;
+        input logic [31:0] exp_read1, exp_read2;
     begin
         @(negedge clk);
             if (exp_read1 == regOut1) begin
                 $info("Correct exp_read1 value!");
             end else begin
-                $error("Incorrect exp_read1 value :(. Actual: %0d, Expected: %0d.", readReg1, exp_read1);
+                $error("Incorrect exp_read1 value :(. Actual: %0d, Expected: %0d.", regOut1, exp_read1);
             end
             if (exp_read2 == regOut2) begin
                 $info("Correct exp_read2 value!");
             end else begin
-                $error("Incorrect exp_read2 value :(. Actual: %0d, Expected: %0d.", readReg2, exp_read2);
+                $error("Incorrect exp_read2 value :(. Actual: %0d, Expected: %0d.", regOut2, exp_read2);
             end
     end
     endtask
 
     task toggle_rst; 
-        rst = 0; #10;
         rst = 1; #10;
         rst = 0; #10;
+        rst = 1; #10;
     endtask
 
 endmodule
