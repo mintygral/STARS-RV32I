387c80726928a0ba4944e1fe4c8f2794c399096b
mintygral <mailmedhashinde@gmail.com>
2024-07-02 09:15:52 -0400

    changed clk to clk_divider

--- source/temp_sensor.sv
+++ source/temp_sensor.sv
@@ -13,20 +13,23 @@ module temp_sensor (
     input logic clk, rst, 
     input logic read_request,
     // outputs
-    output logic read_command, read_signal, read_clk, out_wire,
+    output logic out_wire,
     output state_t state
     );
-    logic [3:0] fcount; // read state
-    logic [2:0] tcount;
-    logic [2:0] rcount; // every clk cycle
-    logic [6:0] clk_divider; // writing state, when rcount == 7
+
+    logic [3:0] count_read; // read state
+    logic [2:0] count_index; // index
+    logic [2:0] count_reg; // every clk cycle
+    logic [6:0] clk_divider; // writing state, when count_reg == 7
     state_t next_state, prev_state;
 
     logic [7:0] skip_rom = 8'hCC;
     logic [7:0] convert_t = 8'h44;
     logic [7:0] read_scratch = 8'hBE;
 
-    always_ff @(posedge clk, posedge rst) begin : startFSM
+    logic strobe = (clk_divider == 100);
+
+    always_ff @(posedge strobe, posedge rst) begin : startFSM
         if (rst) begin
             state <= IDLE;
         end else begin
@@ -37,88 +40,91 @@ module temp_sensor (
     always_comb begin : changeState
         next_state = state;
         prev_state = state;
-        fcount = 0;
-        tcount = 0;
-        rcount = 0;
+        count_read = 0;
+        count_index = 0;
+        count_reg = 0;
+        clk_divider = 0;
         out_wire = 0;
         case(state)
             IDLE: begin
-                fcount = 0;
+                count_read = 0;
                 if (read_request) begin
                     next_state = SKIP_ROM;
                 end
                 else begin
                     next_state = IDLE;
-                    fcount = 0;
+                    count_read = 0;
                 end
             end
             SKIP_ROM: begin
-                rcount = 0;
-                if (rcount == 7) begin
+                count_reg = 0;
+                if (count_reg == 7) begin
+                    clk_divider++;
                     next_state = CONVERT_TEMP;
                 end
                 else begin
-                    out_wire = skip_rom[tcount]; 
-                    tcount++;
-                    rcount++;
+                    out_wire = skip_rom[count_index]; 
+                    count_index++;
+                    count_reg++;
                     next_state = SKIP_ROM;
                 end
             end
             CONVERT_TEMP: begin
-                rcount = 0; 
-                if (rcount == 7) begin
+                count_reg = 0; 
+                if (count_reg == 7) begin
                     next_state = SKIP_ROM2;
                 end
                 else begin 
-                    rcount++;
+                    count_reg++;
                     next_state = CONVERT_TEMP;
                 end
             end
             SKIP_ROM2: begin 
-                rcount = 0;
-                if (rcount == 7) begin
+                count_reg = 0;
+                if (count_reg == 7) begin
+                    clk_divider++;
                     next_state = READ_SCRATCH;
                 end
                 else begin 
-                    rcount++;
+                    count_reg++;
                     next_state = SKIP_ROM2;
                 end
             end
             READ_SCRATCH: begin
-                rcount = 0;
-                if (rcount == 7) begin
+                count_reg = 0;
+                if (count_reg == 7) begin
                     next_state = READ;
                 end
                 else begin 
-                    rcount++;
+                    count_reg++;
                     next_state = READ_SCRATCH;
                 end
             end
             READ: begin
-                fcount = 0;
-                if (fcount == 15) begin
+                count_read = 0;
+                if (count_read == 15) begin
                     next_state = RESET;
                 end
                 else begin 
-                    fcount++;
+                    count_read++;
                     next_state = READ_SCRATCH;
                 end
             end
             RESET: begin
-                fcount = 0;
-                if (fcount == 15) begin
+                count_read = 0;
+                if (count_read == 15) begin
                     next_state = IDLE;
                 end
                 else begin 
-                    fcount++;
+                    count_read++;
                     next_state = RESET;
                 end
             end
             default: begin
                 next_state = IDLE;
-                fcount = 0;
-                tcount = 0;
-                rcount = 0; 
+                count_read = 0;
+                count_index = 0;
+                count_reg = 0; 
             end
 
         endcase
